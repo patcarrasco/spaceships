6d6d8a47e494621358fb6825fad7caa5ab7a0f94
Jesus Escalona <jesusescalona@me.com>
2019-01-20 12:31:11 -0500

    Init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let canvas = document.createElement("canvas");
+  let ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 800;
+  canvas.style.background = "black";
+  document.body.appendChild(canvas);
+
+
+})
