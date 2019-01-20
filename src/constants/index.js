396af9fad0e93160466a125be532e073d3e5c584
github username <patcarrasco@outlook.com>
2019-01-20 16:03:53 -0500

    add api consts

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const API_ROOT = 'http://localhost:3000';
+export const API_WEBSOCK_ROOT = 'ws://localhost:3000/cable';
+export const HEADERS = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
